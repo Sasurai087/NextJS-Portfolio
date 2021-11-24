5d1cc9408e2f8b1197e30ef91f44501c203a55bb
James Ohsako <johsako087@gmail.com>
2021-11-23 16:21:53 -0800

    style: add logo, navbar, and github/linkedin icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,17 +10,58 @@ import {
   Div3,
   NavLink,
   SocialIcons,
+  Span,
 } from "./HeaderStyles";
 
 const Header = () => (
   <Container>
+    {/* Top left logo */}
     <Div1>
       <Link href="/">
-        <a style={{ display: "flex", alignItems: "centered", color: "white" }}>
-          <DiCssdeck size="3rem" /> <span>Portfolio</span>
+        <a
+          style={{
+            display: "flex",
+            alignItems: "center",
+            color: "white",
+            marginBottom: "20px",
+          }}
+        >
+          <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
         </a>
       </Link>
     </Div1>
+
+    {/* Middle navbar */}
+
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+
+      <li>
+        <Link href="#contact">
+          <NavLink>Contact</NavLink>
+        </Link>
+      </li>
+    </Div2>
+
+    {/* Top right social icons */}
+    <Div3>
+      <SocialIcons href="https://github.com">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://linkedin.com">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+    </Div3>
   </Container>
 );
 
